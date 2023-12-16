e886daa6e5a3d706806ba6b7d52c105b328e2495
Hxpe. Dev <104735018+justcallme2k@users.noreply.github.com>
2023-12-16 15:35:25 +0000

    Update Sidebar - styles + BusinessAccounts Import

--- web/src/components/Banking/Components/Sidebar/Sidebar.tsx
+++ web/src/components/Banking/Components/Sidebar/Sidebar.tsx
@@ -1,14 +1,22 @@
 import styles from './Sidebar.module.scss'
+import '../Accounts/BusinessAccounts.module.scss'
+
 import { Accounts } from '../Accounts/Accounts'
+import { BusinessAccounts } from '../Accounts/BusinessAccounts'
 
 export const Sidebar: React.FC<{
     accounts: any
+    businessaccounts: any
     setDepositModalState: any
 }> = (props) => {
     return (
         <div
-            className={styles.sideBar}
+            className={styles.Sidebar}
         >
+            <div className={styles.personalAccountsHeader}>
+                Personal Accounts
+            </div>
+
             {props.accounts.map((data, index) => {
                 return (
                     <Accounts
@@ -18,6 +26,21 @@ export const Sidebar: React.FC<{
                     />
                 )
             })}
+
+            <div className={styles.businessAccountsHeader}>
+                Business Accounts
+            </div>
+            
+            {props.businessaccounts.map((data, index) => {
+                return (
+                    <BusinessAccounts
+                        accountName={data.name}
+                        accountBalance={data.balance}
+                        setDepositModal={props.setDepositModalState}
+                    />
+                )
+            })}
+
         </div>
     )
 }

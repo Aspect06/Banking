560acce19042995073f347ccd3e1653d3c1c3c67
Aspecttt2566 <118882930+Aspecttt2566@users.noreply.github.com>
2023-12-18 20:38:30 +0000

    fix: Banking | Issue where account could go into minus amounts

--- Server/Modules/Actions.lua
+++ Server/Modules/Actions.lua
@@ -3,9 +3,9 @@ RPC.register('Banking:withdrawMoney', function(source, Array)
     local user = exports['Framework']:GetModule('GetPlayer')(src)
 
     if Array.Amount > 0 then
-        if exports["Framework"]:GetBalance(src) >= Array.Amount then
+        if Array.Account == 'Personal Account' then
+            if exports["Framework"]:GetBalance(src) >= Array.Amount then
 
-            if Array.Account == 'Personal Account' then
                 local Personal = MySQL.query.await('SELECT * FROM characters WHERE id = @Id', {
                     ['@Id'] = user['PlayerData']['id']
                 })
@@ -36,27 +36,29 @@ RPC.register('Banking:withdrawMoney', function(source, Array)
                 ['@Id'] = Array.accountId
             })
 
-            SavingsTransactions = json.decode(Savings[1].account_transactions)
+            if Savings[1].account_balance >= Array.Amount then
+                SavingsTransactions = json.decode(Savings[1].account_transactions)
 
-            table.insert(SavingsTransactions, {
-                Account = 'Savings Account ' .. Array.accountId,
-                Comment = Array.Comment,
-                Amount = Array.Amount,
-                Date = Array.Date,
-                Withdraw = true
-            })
+                table.insert(SavingsTransactions, {
+                    Account = 'Savings Account ' .. Array.accountId,
+                    Comment = Array.Comment,
+                    Amount = Array.Amount,
+                    Date = Array.Date,
+                    Withdraw = true
+                })
 
-            exports["Framework"]:AddCash(src, Array.Amount)
+                exports["Framework"]:AddCash(src, Array.Amount)
 
-            exports['oxmysql']:execute('UPDATE savings_accounts SET account_transactions = @Transactions WHERE id = @Id', {
-                ['@Id'] = Array.accountId,
-                ['@Transactions'] = json.encode(SavingsTransactions)
-            })
+                exports['oxmysql']:execute('UPDATE savings_accounts SET account_transactions = @Transactions WHERE id = @Id', {
+                    ['@Id'] = Array.accountId,
+                    ['@Transactions'] = json.encode(SavingsTransactions)
+                })
 
-            exports['oxmysql']:execute('UPDATE savings_accounts SET account_balance = @Balance WHERE id = @Id', {
-                ['@Id'] = Array.accountId,
-                ['@Balance'] = Savings[1].account_balance - Array.Amount
-            })
+                exports['oxmysql']:execute('UPDATE savings_accounts SET account_balance = @Balance WHERE id = @Id', {
+                    ['@Id'] = Array.accountId,
+                    ['@Balance'] = Savings[1].account_balance - Array.Amount
+                })
+            end
         end
     end
 end)
